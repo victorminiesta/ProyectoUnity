ef0453871b03f789eca2dd64d2f165673615876e
victorminiesta <victorminiesta@gmail.com>
2023-04-23 13:04:36 +0200

    preguntas puestas en el script pero con error desconocido

--- script.cs
+++ script.cs
@@ -84,8 +84,6 @@ public class script : MonoBehaviour
         {
             aviso2.SetActive(true);
             aviso.SetActive(false);
-            
-
         }
         
     }
@@ -111,32 +109,75 @@ public class script : MonoBehaviour
             case 3:
                 dado3.SetActive(true);
                 break;
-
-
         }
     }
-    public void realizar(){
+    public void realizar()
+    {
         activarpreguntas.SetActive(true);
         empezarjuego.SetActive(false);
     }
 
-    
+    /*public class Pregunta 
+    {
+        public string pregunta;
+        public string[] respuestas;
+        public int respuestasCorrectas;
+    }
 
+    public List<Pregunta> preguntasGeografia = new List <Pregunta>()
+    {
+        new Pregunta(){
+            pregunta = "¿Cuál es el río más largo de España?",
+            respuestas = new string[] { "Ebro", "Tajo", "Duero", "Guadalquivir"},
+            respuestasCorrectas = 1;
+        },
+        new Pregunta(){
+            pregunta = "¿Cuál es la montaña más alta de la Península Ibérica?",
+            respuestas = new string[] { "Monte Perdido", "Aneto", "Mulhacén", "Teide"},
+            respuestasCorrectas = 3;
+        },
+        new Pregunta(){
+            pregunta = "¿Cuál es el volcán más alto de España?",
+            respuestas = new string[] { "Monte Gorbea", "Teide", "Sierra de Cebollera", "Monte Pico Viejo"},
+            respuestasCorrectas = 2;
+        },
+    }
 
+    public List<Pregunta> preguntasBiologia = new List <Pregunta>()
+    {
+        new Pregunta(){
+            pregunta = "¿Qué es la fotosíntesis y en qué organismos se lleva a cabo?",
+            respuestas = new string[] { "El proceso mediante el cual los organismos producen dióxido de carbono a partir de la energía solar. Se lleva a cabo en animales y plantas.", "El proceso mediante el cual los organismos producen oxígeno a partir de la energía solar. Se lleva a cabo en animales y plantas.", "El proceso mediante el cual los organismos producen energía a partir de la glucosa. Se lleva a cabo en animales y plantas.", "El proceso mediante el cual los organismos producen glucosa a partir de la energía solar. Se lleva a cabo en las plantas y en algunas bacterias."},
+            respuestasCorrectas = 4;
+        },
+        new Pregunta(){
+            pregunta = "¿Cuál es la montaña más alta de la Península Ibérica?",
+            respuestas = new string[] { "Monte Perdido", "Aneto", "Mulhacén", "Teide"},
+            respuestasCorrectas = 4;
+        },
+        new Pregunta(){
+            pregunta = "¿Cuál es el pigmento que da color a la piel humana?",
+            respuestas = new string[] { "Clorofila", "Melanina", "Hemoglobina", "Caroteno"},
+            respuestasCorrectas = 2;
+        },
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public List<Pregunta> preguntasVideojuegos = new List <Pregunta>()
+    {
+        new Pregunta(){
+            pregunta = "¿Qué videojuego se centra en la construcción de estructuras con bloques?",
+            respuestas = new string[] { "Fortnite", "Minecraft", "Apex Legends", "Call of Duty"},
+            respuestasCorrectas = 2;
+        },
+        new Pregunta(){
+            pregunta = "¿Cuál es el personaje principal en el videojuego de aventura 'The Legend of Zelda'?",
+            respuestas = new string[] { "Luigi", "Donkey Kong", "Link", "Zelda"},
+            respuestasCorrectas = 3;
+        },
+        new Pregunta(){
+            pregunta = "¿Cuál es el juego de carreras más popular en la serie de videojuegos 'Mario Kart'?",
+            respuestas = new string[] { "Mario Kart 64", "Mario Kart: Double Dash", "Mario Kart Wii", "Mario Kart 8"},
+            respuestasCorrectas = 4;
+        },
+    }*/
 }
